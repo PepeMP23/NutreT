4041a2005838fd99d4ea28576eb517dbc329e217
Puma23 <80652947+Puma23@users.noreply.github.com>
2023-04-13 05:57:51 -0600

    Orden filtros

--- js/select.js
+++ js/select.js
@@ -5,6 +5,31 @@ $(document).ready(function() {
         $('#selectCiudad').prop('disabled', false);
       } else {
         $('#selectCiudad').prop('disabled', true);
+        $('#selectCiudad').val('');
+        $('#thirdSelect').prop('disabled', true);
+        $('#thirdSelect').val('');
+      }
+    });
+
+    $('#selectCiudad').on('change', function() {
+      var selectedOption = $(this).val();
+      if (selectedOption !== '') {
+        $('#selectEspecialidad').prop('disabled', false);
+      } else {
+        $('#selectEspecialidad').prop('disabled', true);
+        $('#selectEspecialidad').val('');
+      }
+    });
+});
+
+/*
+$(document).ready(function() {
+    $('#selectEstado').on('change', function() {
+      var selectedOption = $(this).val();
+      if (selectedOption !== '') {
+        $('#selectCiudad').prop('disabled', false);
+      } else {
+        $('#selectCiudad').prop('disabled', true);
         $('#selectEspecialidad').prop('disabled', true);
       }
     });
@@ -18,3 +43,4 @@ $(document).ready(function() {
       }
     });
   });
+  */
